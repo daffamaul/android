eacb9459386254a97733adde38c08e5c0e322de9
daffamaul <dffmln2004@gmail.com>
2023-10-16 11:34:06 +0700

    added scrolling application

---
+++ app/src/main/java/com/a4/daffaapps/ScrollingIceCold.java
@@ -0,0 +1,13 @@
+package com.a4.daffaapps;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrollingIceCold extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollicecold);
+    }
+}
